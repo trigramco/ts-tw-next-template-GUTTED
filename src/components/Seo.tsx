99a990f018b07156af169f8c6ec94357b9c9f91a
John Miller <johnrobmiller@gmail.com>
2023-03-31 20:21:58 -0600

    feat: gutted the template

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -15,7 +15,8 @@ const defaultMeta = {
    * No need to be filled, will be populated with openGraph function
    * If you wish to use a normal image, just specify the path below
    */
-  image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
+  // !STARTERCONF add your own image here. This one was gutted in this template
+  // image: 'https://tsnext-tw.thcl.dev/images/large-og.png',
 };
 
 type SeoProps = {
@@ -33,16 +34,6 @@ export default function Seo(props: SeoProps) {
     ? `${props.templateTitle} | ${meta.siteName}`
     : meta.title;
 
-  // Use siteName if there is templateTitle
-  // but show full title if there is none
-  // !STARTERCONF Follow config for opengraph, by deploying one on https://github.com/theodorusclarence/og
-  // ? Uncomment code below if you want to use default open graph
-  // meta['image'] = openGraph({
-  //   description: meta.description,
-  //   siteName: props.templateTitle ? meta.siteName : meta.title,
-  //   templateTitle: props.templateTitle,
-  // });
-
   return (
     <Head>
       <title>{meta.title}</title>
@@ -55,14 +46,16 @@ export default function Seo(props: SeoProps) {
       <meta property='og:site_name' content={meta.siteName} />
       <meta property='og:description' content={meta.description} />
       <meta property='og:title' content={meta.title} />
-      <meta name='image' property='og:image' content={meta.image} />
+      {/* // !STARTERCONF add your own image here. This one was gutted in this template */}
+      {/* <meta name='image' property='og:image' content={meta.image} /> */}
       {/* Twitter */}
       <meta name='twitter:card' content='summary_large_image' />
       {/* // !STARTERCONF Remove or change to your handle */}
       {/* <meta name='twitter:site' content='@th_clarence' /> */}
       <meta name='twitter:title' content={meta.title} />
       <meta name='twitter:description' content={meta.description} />
-      <meta name='twitter:image' content={meta.image} />
+      {/* // !STARTERCONF add your own image here. This one was gutted in this template */}
+      {/* <meta name='twitter:image' content={meta.image} /> */}
       {meta.date && (
         <>
           <meta property='article:published_time' content={meta.date} />
